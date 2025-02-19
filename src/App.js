5832bd6d0690796705be6f26384311926bb2fd81
WhiteDevil162004 <whitedevil162004@outlook.com>
2025-02-19 11:42:32 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Header from './component/Header'
 import Footer from './component/Footer'
+import Banner from './component/Banner'
+
+import Typewritter from './component/Typewritter'
 
 
 
@@ -8,6 +11,7 @@ const App = () => {
   return (
     <div>
    <Header/>
+   <Typewritter/>
    <Footer/>
     </div>
   )
